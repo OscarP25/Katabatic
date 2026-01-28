2d1d48e967131cfe15d2430644a6a5c346cd1a8d
victornyabote <nyabotevictor@gmail.com>
2026-01-28 20:44:19 +1100

    Register synthetic models under katabatic.models

--- katabatic/models/__init__.py
+++ katabatic/models/__init__.py
@@ -3,22 +3,17 @@ Model package for Katabatic.
 
 Exposes the abstract base Model class and the concrete PATEGAN implementation.
 """
+from .copulagan_victor.adapter import CopulaGANAdapter
+from .ctgan_victor.adapter import CTGANAdapter
+from .codi_victor.adapter import CoDiAdapter
+from .ganblr_victor.models import GANBLRModel
+from .medgan_victor.models import MedGANModel
+from .great_victor.models import GReaTModel
+from .gaussiancopula_victor.adapter import GaussianCopulaAdapter
+from .forestdiffusion_victor.adapter import ForestDiffusionAdapter
+from .tvae_victor.adapter import TVAEAdapter
+from .pategan_victor.adapter import PATEGANAdapter
 
-from .base_model import Model
-from .pategan import PATEGAN  # this refers to katabatic/models/pategan/__init__.py
-# GAN-based
-from .ctgan.adapter import CTGANAdapter
-from .copulaGAN.adapter import CopulaGANAdapter
-from .gaussianCopula.adapter import GaussianCopulaAdapter
-from .pategan.adapter import PATEGANAdapter
-from .ganblr.models import GANBLRModel
-from .medgan.models import MedGANModel
-
-# Diffusion / Transformer
-from .codi.models import CoDiModel
-from .forestdiffusion.adapter import ForestDiffusionAdapter
-from .tvae.adapter import TVAEAdapter
-from .great.models import GReaTModel
 
 
 __all__ = ["Model", "PATEGAN"]
